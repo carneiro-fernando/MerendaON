9c0f9d73bc194fed08cf880d83f505aade203eb2
Fernando Carneiro <carneiro.fernando@icloud.com>
2021-05-07 11:54:10 -0300

    Navigation bar added

--- controller/controller.js
+++ controller/controller.js
@@ -1,14 +1,27 @@
 
-function to_another_check_func () {
-    // Get the checkbox
-    var checkBox = document.getElementById("to_another_checkbox");
-    // Get the output text
-    var text = document.getElementById("to_another");
+
+function someone_else_func () {
+    
+  // Get the checkbox
+    var checkBox = document.getElementById("someone_else_checkbox");
   
-    // If the checkbox is checked, display the output text
+  // Get the output div
+    var div_someone_else = document.getElementById("someone_else");
+
+    
+  // If the checkbox is checked, display the output div
     if (checkBox.checked == true){
-      style.display = "block";
+      div_someone_else.style.display = "block";
     } else {
-      style.display = "none";
+      div_someone_else.style.display = "none";
+    }
+  }
+
+  function topNav_resize() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
     }
   }
