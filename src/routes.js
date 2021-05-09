dfd03e066e587bbc3e12afaa1d0d60a6221874e2
carneiro-fernando <carneiro.fernando@icloud.com>
2021-05-09 18:48:51 -0300

    Heroku deployment

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const baseUrl =  "../public/views/";
 const path = require('path');
 
 router.get("/", (req, res) => {res.sendFile(path.join(__dirname, baseUrl, 'index.html'))});
+
 router.get("/register", (req, res) => {res.sendFile(path.join(__dirname, baseUrl, 'register.html'))});
 
 module.exports = router;
