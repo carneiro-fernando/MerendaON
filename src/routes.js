ac0896f699ad0a43f56bfac1e083a6ab34169bde
carneiro-fernando <carneiro.fernando@icloud.com>
2021-05-09 18:21:48 -0300

    routes.js modified

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const baseUrl =  "../public/views/";
 const path = require('path');
 
 router.get("/", (req, res) => {res.sendFile(path.join(__dirname, baseUrl, 'index.html'))});
+router.get("/register", (req, res) => {res.sendFile(path.join(__dirname, baseUrl, 'register.html'))});
 
 module.exports = router;
